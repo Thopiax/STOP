e9c784fcfebbf8bbec38aeaf0da94cda7c282c03
Loic Poulmarck <lpoulmarck@gmail.com>
2020-03-04 17:25:44 +0000

    start of calculation score function

--- backend/room.py
+++ backend/room.py
@@ -6,12 +6,12 @@ class Room:
     def __init__(self, room_id):
         self.players = {}
         self.id = room_id
+        self.round_points = {}
 
     def add_player(self, player):
         self.players[player.id] = player
 
     def start_game(self):
-        self.points = {}
         pass
 
     def start_round(self):
@@ -22,6 +22,13 @@ class Room:
 
     def finish_game(self):
         pass
+
+    def choose_letter(self):
+        pass
+
+    def calculate_score(self, player_answers):
+
+        return self.round_points
 
     def to_json(self):
         return {
