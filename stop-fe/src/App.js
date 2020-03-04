f5be568b5d5fc40becfb81eb4e0346108c40c7ba
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2020-03-04 17:47:36 +0100

    Initial front-end router

--- stop-fe/src/App.js
+++ stop-fe/src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation
+} from "react-router-dom";
+
+function RoomSetup() {
+  return (<div></div>);
+}
+
+function RoomJoin() {
+  return (<div></div>);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>STOP</h1>
+          <div>
+            <Link to="/create" class="button"><button>Create Room</button></Link>
+            <Link to="/join" class="button"><button>Join Room</button></Link>
+          </div>
+        </header>
+      </div>
+
+      <Switch>
+        <Route exact path="/create">
+          <RoomSetup />
+        </Route>
+        <Route exact path="/join">
+          <RoomJoin />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
