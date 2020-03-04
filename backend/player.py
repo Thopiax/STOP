1aa474ab8b92a52aa178ff4e1e27e01515b4a2cf
Loic Poulmarck <lpoulmarck@gmail.com>
2020-03-04 16:56:06 +0000

    add points

--- backend/player.py
+++ backend/player.py
@@ -3,6 +3,8 @@ class Player:
     def __init__(self, room, color):
         self.room = room
         self.id = color
+        self.points = 0
+        self.answers = {}
 
 
     def __repr__(self):
