3dae3c587e45b83efe8d271f3e2271139a4d324f
Norbert Podsadowski <norbo11@googlemail.com>
2020-03-22 16:28:20 +0000

    Improve deployment

---
+++ backend/deploy.sh
@@ -0,0 +1,2 @@
+heroku container:push --app stop-backend web;
+heroku container:release --app stop-backend web;
---
+++ frontend/deploy.sh
@@ -0,0 +1,2 @@
+heroku container:push --app stop-frontend web;
+heroku container:release --app stop-frontend web;
